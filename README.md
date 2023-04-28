0c08a57b47773f67c60c00968f8ea8049b5f2962
jsh9 <25124332+jsh9@users.noreply.github.com>
2023-04-28 02:25:20 -0700

    Add code, icon, and config

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # chrome-tab-merger-splitter
+
+This is a Chrome and Edge extension to split a tab from a browser window, or merge a single-tab window into another browser window.
+
+There is no built-in keyboard shortcut to achieve this in Chrome or Edge, which is why I wrote this extension to improve productivity.
+
+## 1. Installation
+
+(To be published to the Chrome Web Store...)
+
+
+## 2. How to use
+
+* To split a tab from the current browser window, use `Alt + Shift + X` (on macOS: `Option + Shift + X`)
+
+* To merge a single-tab window into anthoer browser window, use `Alt + Shift + Z` (on macOS: `Option + Shift + Z`)
---
+++ background.js
@@ -0,0 +1,31 @@
+chrome.commands.onCommand.addListener(async (command) => {
+  const currentTab = await getCurrentTab();
+  const windows = await chrome.windows.getAll({ populate: true });
+  const currentWindow = windows.find((win) => win.id === currentTab.windowId);
+
+  switch (command) {
+    case 'merge_tab':
+      if (windows.length > 1 && currentWindow.tabs.length === 1) {
+        const otherWindow = windows.find((win) => win.id !== currentTab.windowId);
+        chrome.tabs.move(currentTab.id, { windowId: otherWindow.id, index: -1 });
+      }
+      break;
+
+    case 'split_tab':
+      if (currentWindow.tabs.length > 1) {
+        chrome.windows.create({ tabId: currentTab.id });
+      }
+      break;
+
+    default:
+      break;
+  }
+});
+
+async function getCurrentTab() {
+  return new Promise((resolve) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      resolve(tabs[0]);
+    });
+  });
+}
